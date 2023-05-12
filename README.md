7ae38b713819661069c11e1cbc4d38f46e52211b
Kenneth Bingham <kenneth.bingham@netfoundry.io>
2023-05-12 12:18:08 -0400

    initial

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# template-repo
-Various materials related to or pertaining to the branding of OpenZiti
+# builder container for ziti-edge-tunnel
+
+This container image builds the ziti-edge-tunnel binary from source. This container image is available from Docker Hub as [openziti/ziti-tunnel-sdk-c-builder](https://hub.docker.com/r/openziti/ziti-tunnel-sdk-c-builder).
+
+## Build ziti-edge-tunnel
+
+To build the ziti-edge-tunnel binary, run the following command in the root of the **ziti-tunnel-sdk-c** repository:
+
+```bash
+docker run --rm --volume "${PWD}:/github/workspace" openziti/ziti-tunnel-sdk-c-builder
+```
+
+The default build target is "bundle" which produces a ZIP archive in `./build/package`. The executable binary is also stored in `./build/programs/ziti-edge-tunnel/Release/ziti-edge-tunnel`.
+
+## Cross-compiling
+
+You may build the Linux binary for a different architecture by specifying a CMake prefix from `/ziti-tunnel-sdk-c/CMakePresets.json`. For example, build the Linux binary for ARM64 with the following command:
+
+```bash
+docker run --rm --volume "${PWD}:/github/workspace" openziti/ziti-tunnel-sdk-c-builder ci-linux-arm64
+```
---
+++ docker-image/entrypoint.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+#
+# Ubuntu Bionic 18.04
+#
+
+set -euo pipefail
+
+# these commands must be in the entrypoint so they are run after workspace is mounted on Docker workdir
+echo "INFO: GIT_DISCOVERY_ACROSS_FILESYSTEM=${GIT_DISCOVERY_ACROSS_FILESYSTEM}"
+echo "INFO: WORKDIR=${PWD}"
+echo "INFO: $(git --version)"
+
+# if first positional is an expected arch string then set cmake preset,
+# else use ci-linux-x64 (which actually just uses native/host tools - e.g. not cross compile)
+if [ ${#} -ge 1 ]; then
+    cmake_preset="${1}"
+else
+    cmake_preset="ci-linux-x64"
+fi
+
+if [ ${#} -ge 2 ]; then
+    cmake_config="${2}"
+else
+    cmake_config="Release"
+fi
+
+if [ ${#} -ge 3 ]; then
+    build_target="${3}"
+else
+    build_target="bundle"
+fi
+
+if [ "${build_target}" == "package" ]; then
+    BUILD_DIST_PACKAGES="ON"
+else
+    BUILD_DIST_PACKAGES="OFF"
+fi
+
+# workspace dir for each build env is added to "safe" dirs in global config e.g.
+# ~/.gitconfig so both runner and builder containers trust these dirs
+# owned by different UIDs from that of Git's EUID. This is made necessary
+# by newly-enforced directory boundaries in Git v2.35.2
+# ref: https://lore.kernel.org/git/xmqqv8veb5i6.fsf@gitster.g/
+for SAFE in \
+    /github/workspace \
+    /__w/ziti-tunnel-sdk-c/ziti-tunnel-sdk-c \
+    /mnt ; do
+        git config --global --add safe.directory ${SAFE}
+done
+
+[[ -d ./build ]] && rm -r ./build
+cmake \
+    -E make_directory \
+    ./build  
+cmake \
+    --preset "${cmake_preset}" \
+    -DCMAKE_BUILD_TYPE="${cmake_config}" \
+    -DBUILD_DIST_PACKAGES=${BUILD_DIST_PACKAGES} \
+    -DVCPKG_OVERLAY_PORTS="./vcpkg-overlays/linux-syslibs/ubuntu18" \
+    -S . \
+    -B ./build
+cmake \
+    --build ./build \
+    --config "${cmake_config}" \
+    --target package \
+    --verbose

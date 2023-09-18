e65ec526a9dd79b13d3b902b92991525064bfd79
Kenneth Bingham <kenneth.bingham@netfoundry.io>
2023-09-18 15:36:10 -0400

    thinly wrap exec args as EUID to provide a writable GIT_CONFIG_GLOBAL

---
+++ docker-image/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# duplicate the global gitconfig to a writable location if not root
+if (( UID ));then
+    USER_WRITABLE="${GIT_CONFIG_GLOBAL}-uid-$UID"
+    cp "$GIT_CONFIG_GLOBAL" "$USER_WRITABLE"
+    GIT_CONFIG_GLOBAL="$USER_WRITABLE"
+fi
+
+exec "$@"
